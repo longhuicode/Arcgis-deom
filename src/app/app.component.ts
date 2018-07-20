1a01ac21649ebacb10e20dc22fed995b1142b784
longhuicode <longhuicode@foxmail.com>
2018-07-20 09:15:25 +0800

    arcgis-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as esriLoader from 'esri-loader';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+
+  ngOnInit(): void {
+    esriLoader.loadModules(['esri/views/MapView', 'esri/Map'])
+      .then(([MapView, Map]) => {
+        let webmap = new Map({
+          basemap: "topo"
+        });
+        let view = new MapView({
+          map: webmap,
+          container: 'viewDiv'
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
 }
